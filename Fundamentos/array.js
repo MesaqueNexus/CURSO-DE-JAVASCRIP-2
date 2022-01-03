20623f91ac015c7c0541e2883736d5cbee1439b3
Mesaque Nexus <mesaquenexus@gmail.com>
2022-01-03 15:48:02 -0300

    Second Commit

--- Fundamentos/array.js
+++ Fundamentos/array.js
@@ -1,2 +1,8 @@
 const cores = ['verde', 'vermelho', 'azul']
 console.log(cores[2])
+
+cores[3] = 'amarelo'
+console.log(cores)
+console.log(cores.length) // retorna o tamanho da array
+cores.push('laranja') // Add na Array
+delete cores[1]
---
+++ Fundamentos/objeto.js
@@ -0,0 +1,12 @@
+const prod1 = {}
+prod1.nome = 'Mesaque'
+prod1.idade = 27
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Paulo',
+    idade: 38
+}
+
+console.log(prod2)
